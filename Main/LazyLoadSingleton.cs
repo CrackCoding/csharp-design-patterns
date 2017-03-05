a0699b4a688ce1b4388280c97efcf5eeff89bb90
AlexeyL <levenetsalexey@gmail.com>
2017-03-05 22:55:01 +0300

    added singletone

---
+++ Main/LazyLoadSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    /// <summary>
+    /// Lazy load implementation of singletone
+    /// </summary>
+    public sealed class LazyLoadSingleton
+    {
+        private static readonly Lazy<LazyLoadSingleton> lazy =
+            new Lazy<LazyLoadSingleton>(() => new LazyLoadSingleton());
+
+        /// <summary>
+        /// Empty private constrictor to prevent direct instantiation
+        /// </summary>
+        private LazyLoadSingleton() { }
+
+        /// <summary>
+        /// Property accessor for instance
+        /// </summary>
+        public static LazyLoadSingleton Instance => lazy.Value;
+    }
+}
---
+++ Main/WithoutLockingSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    /// <summary>
+    /// Without locking thread-safe implementation of singletone
+    /// </summary>
+    public sealed class WithoutLockingSingleton
+    {
+        private static readonly WithoutLockingSingleton _instance = new WithoutLockingSingleton();
+
+        /// <summary>
+        /// Static constructor removes BeforeFieldInit on class 
+        /// so static fields not initialized before they are used
+        /// </summary>
+        static WithoutLockingSingleton() { }
+
+        /// <summary>
+        /// Empty private constrictor to prevent direct instantiation
+        /// </summary>
+        private WithoutLockingSingleton() { }
+
+        /// <summary>
+        /// Property accessor for instance
+        /// </summary>
+        public static WithoutLockingSingleton Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
